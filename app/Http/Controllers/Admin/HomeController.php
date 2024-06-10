b995d8836a14675e1946f1bc0291a695911249db
Audi Ramadani <109649966+Audirmdni@users.noreply.github.com>
2024-06-10 16:13:30 +0700

    Audi Update Controller Home

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
@@ -8,5 +9,10 @@ class HomeController extends Controller
     function showDashboard()
     {
         return view('Admin.Dashboard.index');
+    }
+
+    public function index()
+    {
+        return view('admin.dashboard');
     }
 }
